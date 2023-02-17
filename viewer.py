90660eaaada7fd643c4f1b490cae0d402e72b507
Lucas Clemente Vella <lucas.vella@ethereum.org>
2023-02-17 16:48:36 -0300

    Now we have proper legend.

--- viewer.py
+++ viewer.py
@@ -8,7 +8,8 @@ from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 from matplotlib.backends.backend_gtk3 import (
     NavigationToolbar2GTK3 as NavigationToolbar)
-from gi.repository import Gtk
+import matplotlib as mpl
+from gi.repository import Gtk, Gdk, GObject, cairo
 import gi
 gi.require_version('Gtk', '3.0')
 
@@ -33,10 +34,34 @@ def read_cases_data(num, desc):
             yield row
 
 
+class CellRendererLineColor(Gtk.CellRenderer):
+    drawn = GObject.Property(type=bool, default=False)
+    line_color = GObject.Property(
+        type=Gdk.RGBA, default=Gdk.RGBA(1.0, 1.0, 1.0, 1.0))
+
+    def __init__(self):
+        Gtk.CellRenderer.__init__(self)
+
+    def do_render(self, cr, widget, background_area, cell_area, flags):
+        if self.get_property('drawn') and flags & Gtk.CellRendererState.SELECTED:
+            cr.set_source_rgb(1, 1, 1)
+            cr.rectangle(cell_area.x, cell_area.y,
+                         cell_area.width, cell_area.height)
+            cr.fill()
+
+            height = cell_area.y + cell_area.height / 2
+            cr.move_to(cell_area.x, height)
+            cr.line_to(cell_area.x + cell_area.width, height)
+
+            color = self.get_property('line_color')
+            cr.set_source_rgba(*color)
+            cr.stroke()
+
+
 class Viewer:
     def __init__(self):
         runs_store = Gtk.ListStore(int, str)
-        cases_store = Gtk.ListStore(str)
+        cases_store = Gtk.ListStore(str, bool, Gdk.RGBA)
 
         known_cases = dict()
         for run in read_runs():
@@ -45,7 +70,7 @@ class Viewer:
                 # Insert case name in a dict instead of a set to preserve the original order:
                 known_cases[data[0]] = None
         for case_name in known_cases:
-            cases_store.append([case_name])
+            cases_store.append([case_name, False, Gdk.RGBA()])
 
         builder = Gtk.Builder()
         builder.add_from_file("viewer-gui.glade")
@@ -63,6 +88,8 @@ class Viewer:
         cases_view.set_model(cases_store)
         cases_view.append_column(Gtk.TreeViewColumn(
             'Case', text=0, cell_renderer=Gtk.CellRendererText()))
+        cases_view.append_column(Gtk.TreeViewColumn(
+            'Color', drawn=1, line_color=2, cell_renderer=CellRendererLineColor()))
         self.cases_selection = cases_view.get_selection()
         self.cases_selection.select_all()
 
@@ -90,17 +117,17 @@ class Viewer:
 
     def refresh_data(self, *args):
         (cases, sel_cases) = self.cases_selection.get_selected_rows()
-        cases_vals = {cases.get(cases.get_iter(p), 0)[
-            0]: [] for p in sel_cases}
+        cases_vals = (cases.get_iter(p) for p in sel_cases)
+        cases_vals = {cases.get(it, 0)[0]: (it, []) for it in cases_vals}
 
         (runs, sel_runs) = self.runs_selection.get_selected_rows()
         sel_runs = [runs.get(runs.get_iter(p), 0, 1) for p in sel_runs]
         for run in sel_runs:
-            for row in cases_vals.values():
+            for (_, row) in cases_vals.values():
                 row.append(None)
             for case in read_cases_data(*run):
                 try:
-                    row = cases_vals[case[0]]
+                    (_, row) = cases_vals[case[0]]
                 except KeyError:
                     continue
 
@@ -110,10 +137,16 @@ class Viewer:
         x = list(range(len(sel_runs)))
         self.ax.clear()
         self.ax.set_xticks(x, next(zip(*sel_runs)))
-        for (case_name, values) in cases_vals.items():
-            self.ax.plot(x, values, label=case_name)
-        # self.ax.legend()
-        # TODO: display legend as a pixbuf in the TreeView
+        for (case_name, (it, values)) in cases_vals.items():
+            if all(v is None for v in values):
+                cases.set(it, [1], [False])
+                continue
+
+            l = self.ax.plot(x, values, 'o-', label=case_name)[0]
+
+            # Set the color in the list view to render the legend
+            color = Gdk.RGBA(*mpl.colors.to_rgba(l.get_color()))
+            cases.set(it, [1, 2], [True, color])
         # TODO: display line case name upon hovering
 
         self.canvas.draw()
