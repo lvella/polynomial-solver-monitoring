c925aca79a8febaa3e41307221930048a2e9ebb9
Lucas Clemente Vella <lucas.vella@ethereum.org>
2023-02-20 18:00:21 -0300

    Also plotting relative progress for timedout cases.

--- viewer.py
+++ viewer.py
@@ -8,6 +8,7 @@ from gi.repository import Gtk, Gdk, GObject
 import os
 import re
 import csv
+import math
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg as FigureCanvas
 from matplotlib.backends.backend_gtk3 import NavigationToolbar2GTK3 as NavigationToolbar
@@ -106,6 +107,10 @@ class Viewer:
         self.fig = Figure(figsize=(5, 4), dpi=100)
         self.ax = self.fig.add_subplot(1, 1, 1)
 
+        self.radiobutton_plot_runtime = builder.get_object("radiobutton_plot_runtime")
+        self.radiobutton_plot_progress = builder.get_object("radiobutton_plot_progress")
+        self.get_data = self.get_runtime_data
+
         box = builder.get_object("graph_box")
 
         # Add canvas to vbox
@@ -119,12 +124,75 @@ class Viewer:
         box.pack_start(toolbar, False, False, 0)
 
         builder.connect_signals(
-            {"onQuit": Gtk.main_quit, "onDataSelection": self.refresh_data}
+            {
+                "onQuit": Gtk.main_quit,
+                "onDataSelection": self.refresh_data,
+                "onChangePlotType": self.change_plot_type,
+            }
         )
 
         self.refresh_data()
 
         win.show_all()
+
+    def change_plot_type(self, button):
+        if self.radiobutton_plot_runtime.get_active():
+            self.get_data = self.get_runtime_data
+        elif self.radiobutton_plot_progress.get_active():
+            self.get_data = self.get_progress_data
+        self.refresh_data()
+
+    def get_runtime_data(self, selected_runs, selected_cases):
+        for run in selected_runs:
+            for (_, row) in selected_cases.values():
+                row.append(None)
+            for case in read_cases_data(*run):
+                try:
+                    (_, row) = selected_cases[case[0]]
+                except KeyError:
+                    continue
+
+                if case[1] == "Success":
+                    row[-1] = float(case[3]) + float(case[4])
+
+    def get_progress_data(self, selected_runs, selected_cases):
+        for run in selected_runs:
+            for (_, row) in selected_cases.values():
+                row.append((False, None))
+            for case in read_cases_data(*run):
+                try:
+                    (_, row) = selected_cases[case[0]]
+                except KeyError:
+                    continue
+
+                row[-1] = (case[1] == "Timedout", int(case[5]))
+
+        for (_, row) in selected_cases.values():
+            # We are only interested in cases which are themselves timeouts,
+            # and cases immediately before of after timeouts.
+            new_row = []
+            for i in range(len(row)):
+                (is_timeout, value) = row[i]
+                if (
+                    is_timeout
+                    or (i > 0 and row[i - 1][0])
+                    or ((i + 1) < len(row) and row[i + 1][0])
+                ):
+                    new_row.append(value)
+                else:
+                    new_row.append(None)
+
+            # Normalize by the first value in the sequence, and take the log_2
+            first = None
+            for i in range(len(new_row)):
+                if new_row[i] is not None:
+                    if first is None:
+                        first = new_row[i]
+                        new_row[i] = 0.0
+                    else:
+                        new_row[i] = math.log2(new_row[i] / first)
+
+            row[:] = new_row
 
     def refresh_data(self, *args):
         (cases, sel_cases) = self.cases_selection.get_selected_rows()
@@ -133,17 +201,8 @@ class Viewer:
 
         (runs, sel_runs) = self.runs_selection.get_selected_rows()
         sel_runs = [runs.get(runs.get_iter(p), 0, 1) for p in sel_runs]
-        for run in sel_runs:
-            for (_, row) in cases_vals.values():
-                row.append(None)
-            for case in read_cases_data(*run):
-                try:
-                    (_, row) = cases_vals[case[0]]
-                except KeyError:
-                    continue
 
-                if case[1] == "Success":
-                    row[-1] = float(case[3]) + float(case[4])
+        self.get_data(sel_runs, cases_vals)
 
         x = list(range(len(sel_runs)))
         self.ax.clear()
