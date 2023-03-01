e6f0b8b76c405285f633edfe46abf9ff93ddc319
Lucas Clemente Vella <lucas.vella@ethereum.org>
2023-03-01 17:11:26 -0300

    Separating executions by processor and skipping already executed.

--- run-monitoring-benchmarks.py
+++ run-monitoring-benchmarks.py
@@ -6,6 +6,26 @@ import sys
 from subprocess import run
 
 
+def get_cpu_signature():
+    identifying_elements_re = [
+        r"^vendor_id\s*?: (.+?)$",
+        r"^cpu family\s*?: (\d+)",
+        r"^model\s*?: (\d+)",
+        r"^stepping\s*?: (\d+)",
+    ]
+
+    with open("/proc/cpuinfo", "r") as cpuinfo:
+        info = cpuinfo.read()
+
+    signature = []
+    for elem_re in identifying_elements_re:
+        m = re.search(elem_re, info, re.M)
+        if m:
+            signature.append(m.group(1))
+
+    return "_".join(signature)
+
+
 def main():
     script_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -17,8 +37,7 @@ def main():
 
     # Test if git repo is clean
     os.chdir(git_repo)
-    r = run(
-        "git status --untracked-files=no --porcelain".split(), capture_output=True)
+    r = run("git status --untracked-files=no --porcelain".split(), capture_output=True)
     r.check_returncode()
     if r.stdout:
         print("Error: repository is not clean")
@@ -27,24 +46,42 @@ def main():
     run("git fetch --tags".split()).check_returncode()
 
     # Get monitoring tags
-    r = run("git show-ref --tags".split(),
-            capture_output=True)
+    r = run("git show-ref --tags".split(), capture_output=True)
     r.check_returncode()
     tags = [tuple(hash_tag.split()) for hash_tag in r.stdout.splitlines()]
 
     # Filter monitoring tags
     mon_tag_re = re.compile(rb"^refs/tags/(mon.\d+)$")
-    tags = [(hash.decode("ascii"), tag.group(1).decode("ascii")) for (hash, tag) in ((hash, mon_tag_re.match(tag))
-                                                                                     for (hash, tag) in tags) if tag]
+    tags = [
+        (hash.decode("ascii"), tag.group(1).decode("ascii"))
+        for (hash, tag) in ((hash, mon_tag_re.match(tag)) for (hash, tag) in tags)
+        if tag
+    ]
 
-    # Just run each of them:
+    # Create the output directory for this particular processor if it doesn't
+    # exist yet:
+    outdir = os.path.join(script_path, "runs", get_cpu_signature())
+    try:
+        os.makedirs(outdir)
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    # Run each of them that is not already present:
     tmp_filename = os.path.join(script_path, "tmp_out.csv")
     for (hash, tag) in tags:
+        final_path_name = os.path.join(outdir, f"run.{tag}.{hash}.csv")
+        if os.path.exists(final_path_name):
+            print(f"Skipping tag {tag} ({hash}) because it is already there")
+            continue
+        else:
+            print(f"Computing tag {tag} ({hash})")
+
         run(["git", "checkout", hash]).check_returncode()
         with open(tmp_filename, "wb") as output:
             r = run("cargo bench --bench monitoring".split(), stdout=output)
         if r.returncode == 0:
-            os.rename(tmp_filename, f"run.{tag}.{hash}.csv")
+            os.rename(tmp_filename, final_path_name)
         else:
             print(f"Something went wrong with tag {tag} ({hash})")
 
